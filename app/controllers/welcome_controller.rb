414d33fe3de057a0e87bb4591db2c7a79b89a870
Mazen Jomaa <jommaz@hotmail.com>
2015-06-02 17:35:23 -0400

    first push for branch

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-class WelcomeController < ActionController::Base
+class WelcomeController < ApplicationController
 
 	def index
 	end
 
 end
-=======
-class WelcomeController < ApplicationController
-
-def index
-end
-
-end
->>>>>>> 4572d16a604cea6253bf7a711303d0dc340eb835
