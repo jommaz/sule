6219416810e465a7caa1f1ac34717c89768edb97
Bradley <showalter.bradley@gmail.com>
2015-06-04 16:57:33 -0400

    Styled some things. Tried to fix time on the show view.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,50 +19,46 @@
 
 
 $(function() {
-    $("#modal-1").on("change", function() {
-        if ($(this).is(":checked")) {
-            $("body").addClass("modal-open");
-        } else {
-            $("body").removeClass("modal-open");
-        }
-    });
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
 
-    $(".modal-fade-screen, .modal-close").on("click", function() {
-        $(".modal-state:checked").prop("checked", false).change();
-    });
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
 
-    $(".modal-inner").on("click", function(e) {
-        e.stopPropagation();
-    });
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
 });
 
 $(document).ready(function(){
-    $(".dropdown-button").click(function() {
-        $(".dropdown-menu").toggleClass("show-menu");
-        $(".dropdown-menu > li").click(function(){
-            $(".dropdown-menu").removeClass("show-menu");
-        });
-        $(".dropdown-menu.dropdown-select > li").click(function() {
-            $(".dropdown-button").html($(this).html());
-        });
-    });
+  $(".dropdown-button").click(function() {
+    $(".dropdown-menu").toggleClass("show-menu");
     
-	$( ".datepicker" ).datepicker({
+    $(".dropdown-menu > li").click(function(){
+      $(".dropdown-menu").removeClass("show-menu");
+    });
+      
+    $(".dropdown-menu.dropdown-select > li").click(function() {
+      $(".dropdown-button").html($(this).html());
+    });
+  });
+    
+	$(".datepicker").datepicker({
 		dateFormat: 'mm/dd/yy'
 	});
-    var vid = document.getElementById("bgvid");
-    console.log(vid);
-    function vidFade() {
-        vid.classList.add("stopfade");
-    }
 
-    vid.addEventListener('ended', function()
-    {
-// only functional if "loop" is removed
-        vid.pause();
-// to capture IE10
-        vidFade();
-    });
-
-
+  var vid = document.getElementById("bgvid");
+    
+  console.log(vid);
+    
+  function vidFade() {
+    vid.classList.add("stopfade");
+  }
+})
 
